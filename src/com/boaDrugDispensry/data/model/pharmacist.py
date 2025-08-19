cbb1ba504fcb12436f500d13381afd1e769c75ed
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-08-19 20:33:21 +0100

    Refactor Pharmacist class to ensure type annotations and consistent formatting.

--- src/com/boaDrugDispensry/data/model/pharmacist.py
+++ src/com/boaDrugDispensry/data/model/pharmacist.py
@@ -1,49 +1,49 @@
 class Pharmacist:
     def __init__( self ):
-        self.id = ""
-        self.first_name = ""
-        self.last_name = ""
-        self.email = ""
-        self.password = ""
+        self.id: str
+        self.first_name: str
+        self.last_name: str
+        self.email: str
+        self.password: str
 
 
     @property
-    def id( self ):
+    def id(self):
         return self.id
 
     @id.setter
-    def id( self, id ):
+    def id(self,id):
         self.id = id
 
     @property
-    def first_name( self ):
+    def first_name(self):
         return self.first_name
 
     @first_name.setter
-    def first_name( self, first_name ):
+    def first_name(self, first_name):
         self.first_name = first_name
 
     @property
-    def last_name( self ):
+    def last_name(self):
         return self.last_name
 
     @last_name.setter
-    def last_name( self, last_name ):
+    def last_name(self, last_name):
         self.last_name = last_name
 
 
     @property
-    def email( self ):
+    def email(self):
         return self.email
 
     @email.setter
-    def email( self, email ):
+    def email(self, email):
         self.email = email
 
     @property
-    def password( self ):
+    def password(self):
         return self.password
 
     @password.setter
-    def password( self, password ):
+    def password(self, password):
         self.password = password
